eb68aa019229ec18950bd7b93c01a280eebc1481
Alejandro Leone <dantofema@gmail.com>
2025-01-07 15:14:58 -0300

    fix - categories

--- resources/views/livewire/top-nav.blade.php
+++ resources/views/livewire/top-nav.blade.php
@@ -4,7 +4,7 @@
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
          aria-label="Global">
         <div class="flex lg:flex-1">
-            <a href="{{ $categories[0]['href'] }}"
+            <a href="{{ config('app.url') }}"
                class="-m-1.5 p-1.5">
                 <span class="sr-only">{{ $companyName }}</span>
                 <img class="h-16 w-auto"
